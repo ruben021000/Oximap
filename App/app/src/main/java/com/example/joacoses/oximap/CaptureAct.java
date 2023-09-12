56e9a1433d77684701ae709ecaa157a2f7f4039f
Rubén Marín <55706771+ruben021000@users.noreply.github.com>
2023-09-12 17:53:08 +0200

    Add files via upload

---
+++ App/app/src/main/java/com/example/joacoses/oximap/CaptureAct.java
@@ -0,0 +1,9 @@
+package com.example.joacoses.oximap;
+
+
+//actividad necesaria para que el escaneo se haga verticalmente
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class CaptureAct extends CaptureActivity {
+}
